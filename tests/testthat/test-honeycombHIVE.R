36e3ea47304caa53dd78aca403d377f4e61ea3cb
theHumanBorch <ncborch@gmail.com>
2025-01-20 04:11:37 -0600

    update testthat

--- tests/testthat/test-honeycombHIVE.R
+++ tests/testthat/test-honeycombHIVE.R
@@ -18,12 +18,11 @@ test_that("honeycombHIVE runs successfully for clustering task", {
   expect_type(res, "list")
   expect_length(res, 3) # 3 layers
   for (layer in res) {
-    expect_named(layer, c("antibodies", "assignments", "task"))
+    expect_named(layer, c("antibodies", "assignments", "task", "membership"))
     expect_equal(layer$task, "clustering")
-    expect_true(is.matrix(layer$antibodies))
+    expect_true(is.data.frame(layer$antibodies))
     expect_true(is.integer(layer$assignments))
-    expect_equal(nrow(layer$antibodies), 15) # nAntibodies
-    expect_equal(length(layer$assignments), nrow(X))
+    expect_equal(length(layer$membership), 150)
   }
 })
 
@@ -69,18 +68,18 @@ test_that("honeycombHIVE runs successfully for regression task", {
                        layers = 3, 
                        nAntibodies = 15, 
                        beta = 5, 
-                       maxIter = 5)
+                       maxIter = 5,
+                       verbose = FALSE)
   
   # Test structure and results
   expect_type(res, "list")
   expect_length(res, 3) # 3 layers
   for (layer in res) {
-    expect_named(layer, c("antibodies", "assignments", "task"))
+    expect_named(layer, c("antibodies", "assignments", "task", "predictions", "membership"))
     expect_equal(layer$task, "regression")
-    expect_true(is.matrix(layer$antibodies))
+    expect_true(is.data.frame(layer$antibodies))
     expect_true(is.numeric(layer$assignments))
-    expect_equal(nrow(layer$antibodies), 15) # nAntibodies
-    expect_equal(length(layer$assignments), nrow(X))
+    expect_equal(length(layer$predictions), 506)
   }
 })
 
@@ -96,7 +95,8 @@ test_that("honeycombHIVE handles different affinity functions", {
                            layers = 2, 
                            nAntibodies = 10, 
                            affinityFunc = aff, 
-                           maxIter = 5)
+                           maxIter = 5,
+                           verbose = FALSE)
     )
     expect_length(res, 2)
     for (layer in res) {
@@ -117,7 +117,8 @@ test_that("honeycombHIVE handles different distance functions", {
                            layers = 2, 
                            nAntibodies = 10, 
                            distFunc = dist, 
-                           maxIter = 5)
+                           maxIter = 5,
+                           verbose = FALSE)
     )
     expect_length(res, 2)
     for (layer in res) {
